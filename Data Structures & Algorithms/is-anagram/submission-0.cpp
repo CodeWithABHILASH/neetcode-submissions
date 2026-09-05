bcd8bd704fb29603314582637b07a077cf20c4f4
ABHILASH A <78740244+CodeWithABHILASH@users.noreply.github.com>
2026-09-05 09:59:27 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int>mp1;
+        map<char,int>mp2;
+        if(s.size()>t.size() or s.size()<t.size()) return false;
+
+        for(int i=0;i<s.size();i++){
+            mp1[s[i]]++;
+            mp2[t[i]]++;
+        }
+        if(mp1==mp2) return true;
+        return false;
+
+    }
+};
