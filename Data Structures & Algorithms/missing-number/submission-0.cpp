18a7c7376e3d3125de8837e1e7a7c2ad20e4dcf3
ABHILASH A <78740244+CodeWithABHILASH@users.noreply.github.com>
2026-09-15 21:42:30 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n= nums.size();
+        int actualSum= n* (n+1)/2;
+        int currentSum=0;
+        for(auto i:nums){
+            currentSum+=i;
+        }
+
+        return actualSum-currentSum;
+
+    }
+};
